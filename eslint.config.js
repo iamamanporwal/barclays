f6970870d8db9fe63942a35b9ddf9f172e239444
porwa1 <aman.porwal@nurturev.com>
2025-06-22 22:14:25 +0530

    yes

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,11 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {'ts-ignore': 'allow-with-description'},
+      ],
+
     },
   },
 )
